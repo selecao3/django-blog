c8bb31717d8a14442143dc557a6c0d250af72463
kazuha <mami.tomoe4@gmail.com>
2018-03-12 10:10:35 +0900

    commits

--- blog01/models.py
+++ blog01/models.py
@@ -1,16 +1,29 @@
 from django.db import models
+from markdown import markdown
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     published = models.DateTimeField()
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField(upload_to='media/',blank=True)
     body = models.TextField()
 
     def __str__(self):
         return self.title
+    #adminのブログのタイトル名にtitleのstringを返す
 
+
+        #if len(self.body))>30:
+        #は出来ない。
+        #ただしインスタンス変数bodyを使って
+        #body.__str__()とすればbodyをstring化することが出来る。.
     def Body(self):
-        return self.body[:30]
+        if len(self.body.__str__())>30:
+            self.body = markdown(self.body)
+            string = self.body[:30] + "..."
+        else:
+            self.body = markdown(self.body)
+            string = self.body
+        return string
 
 # Create your models here.
